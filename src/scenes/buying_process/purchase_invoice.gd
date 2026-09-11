ddd05ec2f62354e92729567c43a63de6210283fc
SebMatDo <JUANSCABEZASM@hotmail.com>
2026-09-10 20:29:10 -0500

    small bugfix, lbl total duplicated text

--- src/scenes/buying_process/purchase_invoice.gd
+++ src/scenes/buying_process/purchase_invoice.gd
@@ -2,7 +2,9 @@ extends Control
 
 @onready var lbl_subtotal: Label = $VBoxContainer/LblSubtotal
 @onready var lbl_coupon: Label = $VBoxContainer/LblCoupon
+@onready var lbl_total: Label = $VBoxContainer/LblTotal
 @onready var btn_confirm: Button = $VBoxContainer/BtnConfirm
+
 var applied_coupon: Dictionary = {}
 
 func _ready() -> void:
@@ -19,18 +21,15 @@ func _update_invoice() -> void:
 	var base_name: String = GlobalManager.selection["base"]
 	var item_1_quantity: int = GlobalManager.selection["item_1"]
 	var item_2_quantity: int = GlobalManager.selection["item_2"]
-
 	var base_price: int = GlobalManager.prices[base_name]
 	var item_1_price: int = GlobalManager.prices["item_1"]
 	var item_2_price: int = GlobalManager.prices["item_2"]
-
 	var item_1_total: int = item_1_quantity * item_1_price
 	var item_2_total: int = item_2_quantity * item_2_price
-
 	lbl_subtotal.text = (
-		"BASE ELEGIDA: %s    $%d\n"
-		+ "INGREDIENTE 1: %d × $%d    $%d\n"
-		+ "INGREDIENTE 2: %d × $%d    $%d\n"
+		"BASE ELEGIDA: %s $%d\n"
+		+ "INGREDIENTE 1: %d × $%d $%d\n"
+		+ "INGREDIENTE 2: %d × $%d $%d\n"
 		+ "--------------------------------\n"
 		+ "Subtotal: $%d"
 	) % [
@@ -50,7 +49,5 @@ func _update_invoice() -> void:
 	if not applied_coupon.is_empty():
 		discount = applied_coupon["value"]
 	var final_total: int = max(subtotal - discount, 0)
-	lbl_coupon.text = "Descuento aplicado por cupón: $%d\nTOTAL: $%d" % [
-		discount,
-		final_total
-	]
+	lbl_coupon.text = "Descuento aplicado por cupón: $%d" % discount
+	lbl_total.text = "TOTAL: $%d" % final_total
